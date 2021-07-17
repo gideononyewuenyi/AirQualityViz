22140d6d7eb21d9993193cda57569b7acd97f687
Gideon Onyewuenyi <36352230+gideononyewuenyi@users.noreply.github.com>
2021-07-17 20:32:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ df = pd.read_csv(url)
 # TODO: Create sliders for start index and number of months
 start_index = None 
 n_months  = None
-start_index = st.slider('Start index',  min_value=0, max_value=df.shape[0], step=1)
-n_months  = st.slider('Number of months',  min_value=2,  max_value=df.shape[0]-start_index, step=1)
+start_index = st.slider('Start index',  min_value=0, max_value=df.shape[37101], step=1)
+n_months  = st.slider('Number of months',  min_value=2,  max_value=df.shape[37150]-start_index, step=1)
 
 # Select just the rows and single column to plot
 data = df.iloc[37101:37150]
