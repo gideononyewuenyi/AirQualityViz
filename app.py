453e8c8d3f5942fafc2ded47f043bc902243125f
Gideon Onyewuenyi <36352230+gideononyewuenyi@users.noreply.github.com>
2021-08-05 15:34:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ df = pd.read_csv(url)
 
 # Select just the rows and single column to plot
 data = df.iloc[37101:37150]
-data = data[['PM2.5_ATM_ug/m3', 'Temperature_F', 'Humidity_%', x = 'Date/Time']]
+data = data[['PM2.5_ATM_ug/m3', 'Temperature_F', 'Humidity_%']]
 
 # TODO: Create a line chart of the data
 st.line_chart(data)
